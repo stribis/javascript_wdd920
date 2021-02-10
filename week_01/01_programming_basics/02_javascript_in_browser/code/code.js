7ec266a8eb6f1cca99664cf0488eacae043d2e7b
Martin Hutchings <martin.hutchings@sae.edu>
2021-02-10 20:35:54 +0100

    initial commit week 1

---
+++ week_01/01_programming_basics/02_javascript_in_browser/code/code.js
@@ -0,0 +1,17 @@
+// My first javascript code example
+/* 
+  This is
+  A 
+  block comment
+*/
+var food = 'popcorn'  // String
+var person = 'Martin'
+
+var age = 29          // Number
+
+
+console.log(person + ' loves ' + food)
+
+if (foo) {
+  console.log('bar')
+}
